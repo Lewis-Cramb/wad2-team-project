ef0c27058b4a9bfd94f6e8e7a9b27a85ca5341ac
“Johnathon <3013701m@student.gla.ac.uk>
2026-03-15 00:03:34 +0000

    Created core views (not auth views) and updated templates

--- RateYourModule/urls.py
+++ RateYourModule/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from RateYourModule import views
 
+app_name = 'rateyourmodule'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('signup/', views.signup, name='signup'),
+    path('profile/<str:username>', views.show_profile, name='show_profile'),
+    path('modules/', views.module_list, name='modules'),
+    path('modules/<str:moduleID>', views.show_module, name='show_module'),
 ]
