d935a93b65d2953c275166e1fc0ae4bbcb827280
“Johnathon <3013701m@student.gla.ac.uk>
2026-03-18 19:10:18 +0000

    Added user registration functionality

---
+++ RateYourModule/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from RateYourModule.models import Module, Review, UserProfile
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    picture = forms.ImageField(required=False)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+
+        if commit:
+            user.save()
+            UserProfile.objects.create(user=user, picture=self.cleaned_data.get("picture"))
+
+        return user
+
+
--- RateYourModule/views.py
+++ RateYourModule/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.db.models import Avg
+from django.urls import reverse
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 from RateYourModule.models import Module
+from RateYourModule.forms import SignUpForm
 
 def index(request):
     modules = Module.objects.annotate(rating=Avg("review__rating")).order_by("-rating")
@@ -15,12 +19,28 @@ def user_login(request):
     return HttpResponse("temp login view")
 
 
+@login_required
 def user_logout(request):
-    return HttpResponse("temp logout view")
+    logout(request)
+    return redirect(reverse('rateyourmodule:index')) 
 
 
 def signup(request):
-    return HttpResponse("temp signup view")
+    if request.method == "POST":
+        form = SignUpForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            return redirect(reverse("rateyourmodule:index"))
+        else:
+            print(form.errors)
+
+    else:
+        form = SignUpForm()
+
+    context_dict = {"form": form}
+    response = render(request, "register.html", context=context_dict)
+    return response 
 
 
 def show_profile(request, username):
